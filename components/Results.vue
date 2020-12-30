632c57ad40d077658263d88bc27c85aaca2af74e
Jordan Buchman <jordan.m.buchman@gmail.com>
2020-12-30 16:52:52 -0700

    First commit

---
+++ components/Results.vue
@@ -0,0 +1,60 @@
+<template>
+  <ul id="matches">
+    <li v-for="match in matches" :key="match.user.album.id">
+      <p> {{match.npr.artist}} - {{match.user.name}} : {{match.npr.title}} is <span v-if="match.npr.ranked"> ranked {{match.npr.rank}} </span> on {{match.npr.list}}</p>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+  import SpotifyWebApi from 'spotify-web-api-js';
+  import npr_data from '~/assets/npr_data.json'
+
+  type SpotifyUserPair= {
+    npr: Object;
+    user: Object;
+  }
+  
+  export default {
+    data () {
+      return {
+        matches: [] as Array<SpotifyUserPair>
+      }
+    },
+    created() {
+      this.getNPRMatches();
+    },
+    methods: {
+      async getDataFromSpotify(){
+        let hash = this.$route.hash;
+        let access_token = hash.substring(1).split('&').map(pair => pair.split('=')).filter(pair => pair[0] === 'access_token')[0][1];
+
+        let spotify = new SpotifyWebApi();
+        spotify.setAccessToken(access_token);
+
+        let top_tracks = await spotify.getMyTopTracks({limit: 50});
+
+        return top_tracks.items;
+
+      },
+
+      async getNPRMatches() {
+        const matches: Array<SpotifyUserPair> = [];
+
+        const spotify_data = await this.getDataFromSpotify();
+
+        spotify_data.forEach(track => {
+          if (Object.keys(npr_data).includes(track.album.id)){
+            matches.push({
+              npr: npr_data[track.album.id],
+              user: track
+            })
+          }
+        });
+
+        this.matches = matches;
+
+      }
+    }
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">
+        npr-core
+      </h1>
+      <div class="links">
+        <a
+          href="https://accounts.spotify.com/authorize?client_id=d58342257cb443e6847a98d9a0476bbf&redirect_uri=http:%2F%2Flocalhost:3000%2Fspotify&scope=user-top-read&response_type=token&state=123"
+          target="_self"
+          class="button--green"
+        >
+          Log in with Spotify
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          Last.fm
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
