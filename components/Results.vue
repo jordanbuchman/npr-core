5580ccc88084eeb597c0950f0aa9de9c42b372de
Jordan Buchman <jordan.m.buchman@gmail.com>
2021-01-13 16:56:45 -0500

    Replace scoring with percentile-based system

--- components/Results.vue
+++ components/Results.vue
@@ -76,6 +76,14 @@
     npr: Object;
     user: SpotifyApi.ArtistObjectFull;
   }
+
+  const percentile = (arr: any, val: any) =>
+  (100 *
+    arr.reduce(
+      (acc: any, v: any) => acc + (v < val ? 1 : 0) + (v === val ? 0.5 : 0),
+      0
+    )) /
+  arr.length;
   
   export default {
     props: {
@@ -161,7 +169,7 @@
     computed: {
       score() {
         let s = (this['track_matches'] as []).length + (this['artist_matches'] as []).length;
-        return Math.round(100/(Math.pow(100, 1/1.5))*Math.pow(s, 1/1.5)) / 100;
+        return Math.floor(percentile([3, 3, 3, 4, 4, 4, 5, 5, 6, 7, 7, 7, 7, 7, 8, 10, 10, 10, 11, 11, 11, 12, 12, 14, 14, 14, 14, 14, 15, 15, 15, 15, 15, 15, 16, 16, 16, 16, 16, 16, 16, 16, 16, 17, 17, 18, 18, 18, 18, 18, 18, 19, 20, 20, 21, 22, 22, 22, 22, 22, 23, 23, 23, 23, 23, 24, 24, 24, 25, 25, 26, 26, 26, 27, 27, 27, 27, 27, 27, 28, 29, 29, 29, 29, 30, 31, 31, 31, 31, 32, 32, 32, 33, 33, 33, 34, 34, 34, 34, 35, 35, 36, 36, 36, 37, 38, 39, 39, 39, 40, 40, 41, 41, 43, 43, 43, 43, 45, 45, 45, 45, 46, 65], s))/100
       },
       verdict() {
         const verdicts = ["No Things Considered", "Obama is your favorite president", "You probably say you like \"indie music\"", "You own at least one NPR tote bag", "You're literally Ira Glass"];
