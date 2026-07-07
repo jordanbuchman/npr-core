dae3204276cafc511f8a7b1a3081a75cd1b13ca3
Samuel Lowe <samlowe106@gmail.com>
2026-07-07 05:41:13 -0400

    Updated Sam's name and link

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -8,7 +8,7 @@
         <strong>How NPRcore are you?</strong> was made with
         <UIcon name="i-lucide-music" class="align-middle" /> by
         <ULink to="https://github.com/jordanbuchman" target="_blank">Jordan Buchman</ULink> and
-        <ULink to="https://github.com/samlowe106" target="_blank">Sam Lowe</ULink>. The
+        <ULink to="https://samlowe.dev" target="_blank">Samuel Lowe</ULink>. The
         <ULink to="https://github.com/jordanbuchman/npr-core" target="_blank">source code</ULink>
         is licensed
         <ULink to="https://opensource.org/licenses/GPL-3.0" target="_blank">GPLv3</ULink>. NPR and
